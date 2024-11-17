e5f88a16e71e8f5ff4eb3c103865fc75962cc45b
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-17 14:03:06 +0530

    Add files via upload

---
+++ Pattern/programs/level1/Pattern4.java
@@ -0,0 +1,11 @@
+package Pattern.programs.level1;
+public class Pattern4 {
+public static void main(String[]args)
+{
+	for(int i=0;i<=5;i++)
+	{
+		System.out.print("*");
+	}
+	System.out.println();
+}
+}
