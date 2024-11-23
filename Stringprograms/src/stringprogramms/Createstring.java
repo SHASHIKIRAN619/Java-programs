2b9075aa30ba5f0b5753b309ef43308f99d20496
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2024-11-23 13:55:52 +0530

    Add files via upload

---
+++ Stringprograms/src/stringprogramms/Createstring.java
@@ -0,0 +1,19 @@
+package stringprogramms;
+public class Createstring {
+ public static void main(String[]args)
+ {
+	 String s1 = "RAMA";
+	 System.out.println(s1);
+	 String s2 = new String("SEETHA");
+	 System.out.println(s2);
+	 char ch[]=new char[6];
+	 ch[0]='H';
+	 ch[1]='A';
+	 ch[2]='N';
+	 ch[3]='U';
+	 ch[4]='M';
+	 ch[5]='A';
+
+	 System.out.println(ch);
+ }
+}
